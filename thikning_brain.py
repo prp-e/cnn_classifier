eff10c84699d271efc17601c9a4a08f012428382
Muhammadreza Haghiri <haghiri75@gmail.com>
2020-09-30 18:59:29 +0330

    Adding necessary libraries

---
+++ thikning_brain.py
@@ -0,0 +1,4 @@
+from keras.preprocessing.image import ImageDataGenerator 
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D, Activation, Dropout, Flatten, Dense 
+from keras import backend as K 
