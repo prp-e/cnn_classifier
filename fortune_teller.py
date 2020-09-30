533f9730870ac8e44365082de1db05b206a6fc33
Muhammadreza Haghiri <haghiri75@gmail.com>
2020-10-01 00:02:30 +0330

    Now I call her Martha

--- fortune_teller.py
+++ fortune_teller.py
@@ -1,7 +1,19 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
 from keras.models import load_model
 from keras.preprocessing import image 
+import numpy as np 
 
 model = load_model('my_model')
-model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
-print(model)
+test_image = image.load_img('test_data/dog3.jpg', target_size=(224, 224))
+test_image = image.img_to_array(test_image)
+test_image = np.expand_dims(test_image, axis=0)
+
+result = model.predict(test_image)
+
+if result[0][0] // 1 == 0: 
+    print("Cat")
+else:
+    print("Dog")
