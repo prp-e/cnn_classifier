d32ad516b742d6bce0c689dfd38aa57033f7df4d
Muhammadreza Haghiri <haghiri75@gmail.com>
2020-09-30 23:08:14 +0330

    Now it understands the freaking model

--- fortune_teller.py
+++ fortune_teller.py
@@ -1,5 +1,7 @@
 from keras.models import load_model
 from keras.preprocessing import image 
 
-model = load_model('saved_model.h5')
+model = load_model('my_model')
 model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+print(model)
