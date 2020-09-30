e7f560e44f0571e301a31f33c68fba05894ef687
Muhammadreza Haghiri <haghiri75@gmail.com>
2020-09-30 21:58:48 +0330

    It'll better go and tell us her predictions

--- fortune_teller.py
+++ fortune_teller.py
@@ -1,3 +1,5 @@
 from keras.models import load_model
 from keras.preprocessing import image 
 
+model = load_model('saved_model.h5')
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
