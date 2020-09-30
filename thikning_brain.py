fc3facde7fa7b8fcfc4541552097bdc2ed1a7b55
Muhammadreza Haghiri <haghiri75@gmail.com>
2020-09-30 19:02:07 +0330

    Needed variables are set

--- thikning_brain.py
+++ thikning_brain.py
@@ -2,3 +2,12 @@ from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential
 from keras.layers import Conv2D, MaxPooling2D, Activation, Dropout, Flatten, Dense 
 from keras import backend as K 
+
+image_height, image_width = 224, 224 
+
+train_data_dir = 'dataset/train_data'
+validation_data_dir = 'dataset/validation_data'
+train_samples = 11500 
+validation_samples = 1000
+epochs = 10
+batch_size = 16
