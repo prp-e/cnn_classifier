8c0ed78b43807694d004dff4d3f2f7de8f237d8f
Muhammadreza Haghiri <haghiri75@gmail.com>
2020-11-17 01:44:47 +0330

    Did a better job on input pics

--- fortune_teller.py
+++ fortune_teller.py
@@ -4,10 +4,15 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 from keras.models import load_model
 from keras.preprocessing import image 
 import numpy as np 
+import argparse 
 
+parser = argparse.ArgumentParser() 
+parser.add_argument('--pic', required=True) 
+pic = parser.parse_args() 
+pic = pic.pic 
 model = load_model('my_model')
 
-test_image = image.load_img('dataset/test_data/not-food/photos-167.jpg', target_size=(224, 224))
+test_image = image.load_img(pic , target_size=(224, 224))
 test_image = image.img_to_array(test_image)
 test_image = np.expand_dims(test_image, axis=0)
 
