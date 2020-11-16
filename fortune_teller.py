ee5d3b8ac2d4840e0433680889dcdcd616ad61c5
Muhammadreza Haghiri <haghiri75@gmail.com>
2020-11-17 01:39:58 +0330

    Finished the backend, somehow

--- fortune_teller.py
+++ fortune_teller.py
@@ -7,14 +7,14 @@ import numpy as np
 
 model = load_model('my_model')
 
-test_image = image.load_img('test_data/dog2.jpg', target_size=(224, 224))
+test_image = image.load_img('dataset/test_data/not-food/photos-167.jpg', target_size=(224, 224))
 test_image = image.img_to_array(test_image)
 test_image = np.expand_dims(test_image, axis=0)
 
 result = model.predict(test_image)
-print(result)
+#print(result)
 
 if result[0][0] // 1 == 0: 
-    print("Cat")
+    print("Food")
 else:
-    print("Dog")
+    print("Not Food")
